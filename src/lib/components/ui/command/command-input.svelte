34399b32aaa03b2858ac993c8c29f84e5a73834d
Argerin Quijano <argie4729@gmail.com>
2026-04-13 13:24:04 +0800

    sveltekit default structure

---
+++ src/lib/components/ui/command/command-input.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Command as CommandPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
+	import * as InputGroup from "$lib/components/ui/input-group/index.js";
+	import MagnifyingGlassIcon from 'phosphor-svelte/lib/MagnifyingGlass';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		value = $bindable(""),
+		...restProps
+	}: CommandPrimitive.InputProps = $props();
+</script>
+
+<div data-slot="command-input-wrapper" class="border-b pb-0">
+	<InputGroup.Root class="bg-input/30 border-input/30 h-8 border-none shadow-none! *:data-[slot=input-group-addon]:pl-2!">
+		<CommandPrimitive.Input
+			data-slot="command-input"
+			class={cn(
+				"w-full text-xs outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
+				className
+			)}
+			bind:ref
+			{...restProps}
+			bind:value
+		/>
+		<InputGroup.Addon>
+			<MagnifyingGlassIcon class="size-4 shrink-0 opacity-50" />
+		</InputGroup.Addon>
+	</InputGroup.Root>
+</div>
---
+++ src/lib/components/ui/pagination/pagination-previous.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { ComponentProps } from "svelte";
+	import { cn } from "$lib/utils.js";
+	import { PaginationLink } from "./index.js";
+	import CaretLeftIcon from 'phosphor-svelte/lib/CaretLeft';
+
+	type PaginationPreviousProps = ComponentProps<typeof PaginationLink>;
+
+	let { class: className, ...restProps }: PaginationPreviousProps = $props();
+</script>
+
+<PaginationLink
+	aria-label="Go to previous page"
+	size="default"
+	class={cn("pl-1.5!", className)}
+	{...restProps}
+>
+	<CaretLeftIcon data-icon="inline-start" />
+	<span class="cn-pagination-previous-text hidden sm:block">Previous</span>
+</PaginationLink>
